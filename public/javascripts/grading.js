0cfd4c9964a5f5989e8cddbac8516ab78761f82c
Brandão <zemariabrandao@gmail.com>
2020-12-22 10:27:50 +0000

    2º Javascript Tutorial

--- public/javascripts/grading.js
+++ public/javascripts/grading.js
@@ -1,3 +1,17 @@
+var students = [
+    { name: "Mary", number: 50037237 },
+    { name: "John", number: 50037238 },
+    { name: "Anthony", number: 50037239 }
+];
+
+var units = [
+    { unit: "Mathematics", semester: 3, etcs: 6 },
+    { unit: "Literature", semester: 2, etcs: 6 },
+    { unit: "Laws", semester: 1, etcs: 3 },
+    { unit: "Informatics", semester: 1, etcs: 6 },
+    { unit: "Cooking", semester: 2, etcs: 3 }
+];
+
 function resetInputs(){
     document.getElementById("student_name").value = "";
     document.getElementById("unit_name").value = "";
@@ -9,14 +23,14 @@ function resetInputs(){
 }
 
 function calculate(){
-    let student_name = document.getElementById("student_name").value;
-    let unit_name = document.getElementById("unit_name").value;
+    let student_id = document.getElementById("student_name").value;
+    let unit_id = document.getElementById("unit_name").value;
     let project_grade = document.getElementById("project_grade").value;
     let percentage_project = document.getElementById("percentage_project").value;
     let test_grade = document.getElementById("test_grade").value;
     let percentage_test = document.getElementById("percentage_test").value;
 
-    if (student_name != "" || unit_name != "" || project_grade != "" || percentage_project != "" || test_grade != "" || percentage_test != "") {
+    if (student_id != "" || unit_id != "" || project_grade != "" || percentage_project != "" || test_grade != "" || percentage_test != "") {
 
         let soma = parseFloat(percentage_test) + parseFloat(percentage_project);
 
@@ -25,7 +39,7 @@ function calculate(){
             let final_grade = (project_grade * percentage_project) + (test_grade * percentage_test);
 
             let aux = "";
-            aux += "Student " + student_name + " obtained " + final_grade.toFixed(1) + " on the " + unit_name + " unit.";
+            aux += "Student " + students[student_id].name + "with number " + students[student_id].number  + " obtained " + final_grade.toFixed(1) + " on the " + units[unit_id].unit + " unit" + " (" + units[unit_id].etcs + " ETCS) of the " + units[unit_id].semester + "º Semester";
             document.getElementById("results").innerHTML = aux;
 
         } else {
@@ -35,4 +49,20 @@ function calculate(){
     } else {
         window.alert("fill in the spaces above!");
     }
+}
+
+window.onload = function(){
+
+    let aux = "";
+    let auxx = "";
+    for (let i in students){
+        aux += "<option value ='" + i + "'>" + students[i].name + "</option>";
+    }
+
+    for (let i in units){
+        auxx += "<option value ='" + i + "'>" + units[i].unit + "</option>";
+    }
+
+    document.getElementById("student_name").innerHTML = aux;
+    document.getElementById("unit_name").innerHTML = auxx;
 }
--- public/javascripts/studentGrades.js
+++ public/javascripts/studentGrades.js
@@ -1,37 +1,61 @@
     var nome = "John Smith";
     nome += " grades";
     
+    var units = [
+        { unit: "Mathematics", grade: 8.3, semester: 3, ects: 6 },
+        { unit: "Literature", grade: 11.2, semester: 2, ects: 6 },
+        { unit: "Laws", grade: 18.5, semester: 1, ects: 3 },
+        { unit: "Informatics", grade: 14.3, semester: 1, ects: 6 },
+        { unit: "Cooking", grade: 7.4, semester: 2, ects: 3 }
+    ];
 
-    var firstUnit = "Mathematics";
-    var firstGrade = 7.5;
-    var secondUnit = "Literature";
-    var secondGrade = 11.2;
+    var sum = 0;
+    var failed = 0;
+    var finished = 0;
+    var ectsSum = 0;
 
     window.onload = function () {
-    document.getElementById("student").innerHTML = nome;
-    let aux = "";
 
-    if(firstGrade < 9.5){
-        aux += "<section class='unitfail'>";
-    }
-    else{
-        aux += "<section class='boxunit'>";
-    }
-    
-    aux += "<div class='square'>" + firstUnit.substring(0, 2) + "</div>";
-    aux += "<section>";
-    aux += "<h2>" + firstUnit + "</h2>";
-    aux += "<h3>" + "Grades: " +  firstGrade + "</h3>";
-    aux += "</section>";
-    aux += "</section>";
-    aux += "<section class='boxunit'>";
-    aux += "<div class='square'>" + secondUnit.substring(0, 2) + "</div>";
-    aux += "<section>";
-    aux += "<h2>" + secondUnit + "</h2>";
-    aux += "<h3>" + "Grades: " + secondGrade + "</h3>";
-    aux += "</section>";
-    aux += "</section>";
-    document.getElementById("grades").innerHTML = aux;
+        document.getElementById("student").innerHTML = nome;
+
+        let html = "";
+        for(let unit of units){
+            html += createUnitHtml(unit);
+        }
+
+        let avarage = sum/finished;
+
+        document.getElementById("grades").innerHTML = html;
+        let aux = ""; 
+        aux += "<summary>" + "Average: " + avarage.toFixed(1) + "</summary>";
+        aux += "<p>" + failed + " failed units" + "</p>";
+        aux += "<p>" + finished + " passed units" + "</p>";
+
+        document.getElementById("summary").innerHTML = aux;
     }
 
+    function createUnitHtml(unit) {
+        let aux = "";
 
+        if(unit.grade < 9.5){
+            aux += "<section class='unitfail'>";
+            failed += 1;
+        }
+        else{
+            aux += "<section class='boxunit'>";
+            finished += 1;
+            sum += unit.grade;
+            ectsSum += unit.ects;
+        }
+        
+        aux += "<div class='square'>" + unit.unit.substring(0, 2) + "</div>";
+        aux += "<section>";
+        aux += "<h2>" + unit.unit + "</h2>";
+        aux += "<h3>" + "Grades: " +  unit.grade + "</h3>";
+        aux += "<h3>" + "Semester: " + unit.semester + "</h3>";
+        aux += "<h3>" + "ECTS: " + unit.ects + "</h3>";
+        aux += "</section>";
+        aux += "</section>";
+
+        return aux;
+    }
